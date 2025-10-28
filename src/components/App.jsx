a7115ab61b1f185cf885b069f58a671cb13050d4
vukp <pukvavlovic10@gmail.com>
2025-10-28 15:17:10 +0100

    added all state that will be used for the project" git status :q git add src/components/App.jsx """
    "

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import Title from "./Title.jsx";
+import GeneralInformation from "./GeneralInformation.jsx";
 
 export default function App() {
+  const [generalInfo, setGeneralInfo] = useState({});
+  const [educationalInfo, setEducationalInfo] = useState([]);
+  const [workExp, setWorkExp] = useState([]);
+  const [isEditingGeneral, setIsEditingGeneral] = useState(false);
+  const [isEditingEdu, setIsEditingEdu] = useState(false);
+  const [isEditingWork, setIsEditingWork] = useState(false);
   return (
     <div>
       <Title />
+      <GeneralInformation />
     </div>
   );
 }
